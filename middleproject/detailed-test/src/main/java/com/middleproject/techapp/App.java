d668b90c51a4fc46b65d2e3a625958300b2e4b1b
Chetwika <52707150+Chetwika@users.noreply.github.com>
2019-07-11 13:40:26 +0530

    Add files via upload

---
+++ middleproject/detailed-test/src/main/java/com/middleproject/techapp/App.java
@@ -0,0 +1,13 @@
+package com.middleproject.techapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
